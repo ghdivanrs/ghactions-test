be463fd041970afa44a5212e73ce139aac99b029
Diego Ivan Rodriguez <108941672+ghdivanrs@users.noreply.github.com>
2022-10-26 15:15:44 -0500

    Update setup.py

--- python-example-app/setup.py
+++ python-example-app/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
 
 setuptools.setup(
     name="example-app-kc",
-    version="0.0.20",
+    version="0.0.21",
     author="Marta Arcones",
     author_email="marta.arcones@gmail.com",
     description="A small example package",
